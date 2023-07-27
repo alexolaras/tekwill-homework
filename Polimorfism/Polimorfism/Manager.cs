8707832674359b52267c45b608185eba6bb9778d
valera1900 <alex.olaras11@gmail.com>
2023-07-27 11:07:42 +0300

    polimorfizm homework

---
+++ Polimorfism/Polimorfism/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfism
+{
+    internal class Manager : Angajat
+    {
+        public Manager(string nume, DateTime dataAngajarii) : base(nume, dataAngajarii)
+        {
+            
+        }
+
+        public override double CalculeazaSalariu()
+        {
+            var salariu = base.CalculeazaSalariu();
+            var exparienta = (int)(DateTime.Now - DataAngajarii).TotalDays/365;
+
+            return salariu + salariu * 0.3 * exparienta;
+        }
+    }
+}
---
+++ Polimorfism/Polimorfism/Program.cs
@@ -0,0 +1,27 @@
+﻿using Polimorfism;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager manager = new Manager("Andrei", new DateTime(2020, 01, 05));
+
+        List<string> limbaje = new List<string> {"C#"};
+        Programator programator = new Programator("Vasile", new DateTime(2022, 06, 22), limbaje);
+
+        Contabil contabil = new Contabil("Valeria", new DateTime(2021, 02, 01));
+
+        var angajati = new List<Angajat>
+        {
+            programator, 
+            contabil, 
+            manager
+        };
+
+        angajati.ForEach(angajat => { Console.WriteLine(angajat.CalculeazaSalariu()); });
+
+        programator.AdaugaLimbaj("php");
+
+        angajati.ForEach(angajat => { Console.WriteLine(angajat.CalculeazaSalariu()); });
+    }
+}
